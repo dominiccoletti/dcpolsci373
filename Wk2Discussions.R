614ef19adbbce6ebe9d94d37a1f51a8338e6e7e2
Dominic Coletti <dcoletti@umich.edu>
2022-01-26 13:14:09 -0500

    more tracking

--- Wk2Discussions.R
+++ Wk2Discussions.R
@@ -20,3 +20,21 @@ View(m1a)
 coord <- c(4,1)
 ###distance formula sqrt((x1 - x2)^2 + (y1 - y2)^2)
 sqrt((coord[1] - length(m1a))^2 + (y1 - y2)^2)
+
+#########Wk 3
+##1
+vec1 <- c(4:50)
+vec1
+
+##2
+max(vec1)
+
+##3
+coord3 <- which(vec1==max(vec1))
+
+##4
+max(vec1[-which.max(vec1)])
+which.max(vec1[-which.max(vec1)])
+
+
+
